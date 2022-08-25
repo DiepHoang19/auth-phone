79c34d94fc6f3d159c0d540e44279cf7e94ddac7
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-08-25 17:48:00 +0700

    first commit

---
+++ React_Firebase_Auth_Tutorial-phone-auth/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA35gdA45Zr1qMAxJS0-q06iyik3tqk1vo",
+  authDomain: "chat-app-path2.firebaseapp.com",
+  projectId: "chat-app-path2",
+  storageBucket: "chat-app-path2.appspot.com",
+  messagingSenderId: "781990468414",
+  appId: "1:781990468414:web:6449c1feffcf3248522a6f",
+  measurementId: "G-2BHVV0GW4G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
